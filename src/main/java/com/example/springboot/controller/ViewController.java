d2409f7e928ce4ec3a86381506402813a228936c
Bobae <bobae.kim@lemonhc.com>
2021-11-18 11:37:20 +0900

    ECMAscript6

--- src/main/java/com/example/springboot/controller/ViewController.java
+++ src/main/java/com/example/springboot/controller/ViewController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @Slf4j
 public class ViewController {
@@ -21,5 +23,17 @@ public class ViewController {
         log.info("appple is {}", b);
         model.addAttribute("apple", b);
         return "view/apple";
+    }
+
+    @RequestMapping("/ecma6**")
+    public String getEcma6(HttpServletRequest request) {
+
+        log.debug("ECMA_VIEW request.getServletPath {}", request.getServletPath());
+        StringBuilder sb = new StringBuilder(request.getServletPath());
+
+        sb.insert(0, "view");
+        log.debug("ECMA_VIEW is {}", sb.toString());
+
+        return sb.toString();
     }
 }
